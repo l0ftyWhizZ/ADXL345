6da03ac78dd077568cff66df1ad10a7f1d8db3b8
djeof-1 <shreyas.srinivasan@iiitb.org>
2016-11-24 12:50:30 +0530

    created a new function to display attributes of the sensor.

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ mainWindow = pygame.display.set_mode((1024,768))
 pygame.display.set_caption("ADXL345: AirPaint")
 
 fps = pygame.time.Clock()
-
+font=pygame.font.SysFont("comicsansms", 20)
 adxl345 = ADXL345()
-print "ADXL345 on address 0x%x:" % (adxl345.address)
 '''
 print "   x = %.3fG" % ( axes['x'] )
 print "   y = %.3fG" % ( axes['y'] )
@@ -19,15 +18,20 @@ print "   z = %.3fG" % ( axes['z'] )
 multi = 20.0
 
 lineList = []
+
+def displayVals():
+    props = font.render("ADXL345 on address 0x"+ str(adxl345.address),1,(255,255,255))
+    mainWindow.blit(props,(20,20))
 while gameLoop:
+    for event in pygame.event.get():
+	if event.type == pygame.QUIT:
+	    pygame.quit()
+            quit()
     axes = adxl345.getAxes(True)
+    displayVals();
     mainWindow.fill((0,100,255))
-    pygame.draw.line(mainWindow, WHITE, [axes['x']*multi, axes['y']*multi],[axes['x']*multi + 1, axes['y']*multi + 1],5);
     lineList.append([axes['x']*multi, axes['y']*multi])
     for i in range(len(lineList)-1):
         pygame.draw.line(mainWindow, WHITE, lineList[i], lineList[i+1],5);
     fps.tick(60)
     pygame.display.flip()
-
-pygame.quit()
-quit()
